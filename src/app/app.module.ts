8c236681e73e047f3fabca09481e3239c6ef5185
chrisperk <christopherperkins.512@gmail.com>
2017-07-13 23:58:26 -0500

    working on routing components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,18 @@ import { RouterModule } from '@angular/router';
 import { AppComponent } from './app.component';
 import { SidebarComponent } from './sidebar/sidebar.component';
 import { CoverComponent } from './cover/cover.component';
+import { WorkComponent } from './work/work.component';
+import { AboutComponent } from './about/about.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     SidebarComponent,
-    CoverComponent
+    CoverComponent,
+    WorkComponent,
+    AboutComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
@@ -22,6 +28,18 @@ import { CoverComponent } from './cover/cover.component';
       {
         path: '**',
         redirectTo: ''
+      },
+      {
+        path: 'work',
+        component: WorkComponent
+      },
+      {
+        path: 'about',
+        component: AboutComponent
+      },
+      {
+        path: 'contact',
+        component: ContactComponent
       }
     ])
   ],
--- src/app/sidebar/sidebar.component.ts
+++ src/app/sidebar/sidebar.component.ts
@@ -11,6 +11,7 @@ export class SidebarComponent {
   linkText: string[] = [
     'Work',
     'About',
+    'Resume',
     'Contact'
   ];
 }
