26120473ff3b00ba88f99b3a0b10c0eb4e9f6aba
chrisperk <christopherperkins.512@gmail.com>
2017-07-16 21:52:09 -0500

    added workbanners to work component

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'contact',
   templateUrl: './contact.component.html',
   // styleUrls: ['./cover.component.css', '../app.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
+  email: string;
+  name: string;
+  subject: string;
+  message: string;
 
+  ngOnInit(): void {
+    this.email = null;
+    this.name = null;
+    this.subject = null;
+    this.message = null;
+  }
 }
