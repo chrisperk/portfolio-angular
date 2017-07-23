353218b2e7e44be0f34a8d6ddd40e0141f56a620
chrisperk <christopherperkins.512@gmail.com>
2017-07-22 23:32:27 -0500

    added responsive sidebar button for mobile view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  
+export class AppComponent implements OnInit {
+  displaySidebar: boolean;
+
+  ngOnInit(): void {
+    this.displaySidebar = false;
+  }
+
+  hideNavMenu() {
+    document.getElementById('sidebar').classList.add('hidden-xs-down');
+    this.displaySidebar = false;
+  }
+
+  revealNavMenu() {
+    document.getElementById('sidebar').classList.remove('hidden-xs-down');
+    this.displaySidebar = true;
+  }
 }
